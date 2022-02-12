45b6a42e867fed00b6593085905087945493751d
Rizal Ilham <rzlilhm09@gmail.com>
2022-02-12 19:23:12 +0700

    Git init

---
+++ tutorial/snippets/serializers.py
@@ -0,0 +1,38 @@
+# class SnippetSerializer(serializers.Serializer):
+#     id = serializers.IntegerField(read_only=True)
+#     title = serializers.CharField(required=False, allow_blank=True, max_length=100)
+#     code = serializers.CharField(style={'base_template': 'textarea.html'})
+#     linenos = serializers.BooleanField(required=False)
+#     language = serializers.ChoiceField(choices=LANGUAGE_CHOISES, default='python')
+#     style = serializers.ChoiceField(choices=STYLE_CHOICES, default='friendly')
+
+#     def create(self, validated_data):
+#         """
+#         Create and return new Snippet instance, given the validated_data
+#         """
+#         return Snippet.objects.create(**validated_data)
+
+#     def update(self, instance, validated_data):
+#         instance.title = validated_data.get('title', instance.title)
+#         instance.code = validated_data.get('code', instance.code)
+#         instance.linenos = validated_data.get('linenos', instance.linenos)
+#         instance.language = validated_data.get('language', instance.language)
+#         instance.style = validated_data.get('style', instance.style)
+        
+#         instance.save()
+#         return instance
+from rest_framework import serializers
+from snippets.models import Snippet, LANGUAGE_CHOISES, STYLE_CHOICES
+from django.contrib.auth.models import User
+
+class SnippetSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Snippet
+        fields = ['id', 'title', 'code', 'linenos', 'language', 'style']
+
+class UserSerializer(serializers.ModelSerializer):
+    snippets = serializers.PrimaryKeyRelatedField(many=True, queryset=Snippet.objects.all())
+    owner = serializers.ReadOnlyField(source='owner.username')
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'snippets', 'owner']
